4236b91eb9e4a9e2681194d09ab5237cc59aada7
yeonsuan <sweetsuan@naver.com>
2021-07-08 23:09:34 +0900

    react component & state

--- again/src/App.js
+++ again/src/App.js
@@ -1,52 +1,30 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC" 
+// TOC를 components파일에 따로 빼두고 import로 써둠
+import Content  from "./components/Content"
 import './App.css';
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-        </ul>
-    </nav>   
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-            <h2>{this.props.title}</h2>
-            {this.props.desc}
-        </article>
-    );
-  }
-}
-
-
-class Subject extends Component {
-  render(){
-    return (
-      <header>
-            <h1>{this.props.title}</h1>
-            {this.props.sub}  
-            {/* 리팩토링방법 */}
-        </header>
-    );
-  }
-}
 
 
 class App extends Component { 
+  constructor(props){
+    super(props);
+    this.state ={
+      Subject:{title:'WEB',sub:'World Wide Web!'},
+      contents:[
+        {id:1 ,title:'HTML', desc:'HTML is ...'},
+        {id:2 ,title:'CSS', desc:'CSS is ...'},
+        {id:3 ,title:'JavaScript', desc:'Javascript is ...'}
+      ]
+    }
+  }
   render(){
     return (
         <div className="App"> 
-          <Subject title="WEB" sub="world wide web!"></Subject>
+          <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
           <Subject title="React" sub="For UI"></Subject>
-          <TOC></TOC>
+          <TOC data={this.state.contents}></TOC>
           <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
         </div>
     );
