999074a36e7f747367e48d4fbbd6ffa036741485
yeonsuan <sweetsuan@naver.com>
2021-07-09 11:04:19 +0900

    task

---
+++ task/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function contact() {
+    return (
+        <div>
+            e-mail
+            <form>
+                <input type="email">
+                <input type="text">
+            </form>
+        </div>
+    )
+}
+
+export default contact;
